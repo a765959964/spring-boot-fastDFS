22f3dde42bde321f1bea660233f25f7d66dcb5aa
zhang765959964 <zhang765959964@qq.com>
2019-04-22 15:58:51 +0800

    fastDFS文件下载

---
+++ src/main/java/com/neo/Test.java
@@ -0,0 +1,12 @@
+package com.neo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		String str  = "M00/00/00/wKjZgFy9UKiAVeObAAGa208ZsGM630.jpg";
+		
+		System.out.println(str.substring(10));
+	}
+
+}
